59f59bea6e5427320012d1be1122a64e615890a8
HEVERHD <hever11.hdgd@gmail.com>
2021-11-23 12:11:44 -0500

    create atritubtes

--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -1,12 +1,38 @@
-const { DataTypes } = require('sequelize');
+cconst { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('country', {
-    name: {
+    id:{
       type: DataTypes.STRING,
       allowNull: false,
-    },
+      primaryKey: true
+  },
+  Name:{
+      type: DataTypes.STRING,
+      allowNull: false
+  },
+  flag:{
+      type: DataTypes.TEXT,
+      allowNull: false
+  },
+  continent:{
+      type: DataTypes.STRING,
+      allowNull: false
+  },
+  capital:{
+      type: DataTypes.STRING,
+      allowNull: false
+  },
+  subregion:{
+      type: DataTypes.STRING
+  },
+  area:{
+      type: DataTypes.INTEGER
+  },
+  population:{
+      type: DataTypes.INTEGER
+  }
   });
 };
