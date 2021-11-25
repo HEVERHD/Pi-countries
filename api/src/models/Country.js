06981b488e4219dc16bf4ebe1ab018740af96a88
HEVERHD <hever11.hdgd@gmail.com>
2021-11-25 11:41:35 -0500

    details back

--- api/src/models/Country.js
+++ api/src/models/Country.js
@@ -2,37 +2,38 @@ const { DataTypes } = require('sequelize');
 // Exportamos una funcion que define el modelo
 // Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
-	// defino el modelo
-	sequelize.define('country', {
-		id: {
-			type: DataTypes.STRING,
-			allowNull: false,
-			primaryKey: true,
-		},
-		Name: {
-			type: DataTypes.STRING,
-			allowNull: false,
-		},
-		flag: {
-			type: DataTypes.TEXT,
-			allowNull: false,
-		},
-		continent: {
-			type: DataTypes.STRING,
-			allowNull: false,
-		},
-		capital: {
-			type: DataTypes.STRING,
-			allowNull: false,
-		},
-		subregion: {
-			type: DataTypes.STRING,
-		},
-		area: {
-			type: DataTypes.INTEGER,
-		},
-		population: {
-			type: DataTypes.INTEGER,
-		},
-	});
+  // defino el modelo
+  sequelize.define('country', {
+    id: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      primaryKey: true
+    }
+    ,name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    flag: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    continent: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    capital: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      defaultValue: 'no capital'
+    },
+    area: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+    poblation: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    }
+  
+  });
 };
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,16 +1,12 @@
 const { Router } = require('express');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
-const countries = require('./countries');
-const activity = require('./activity');
+const countryRouter = require('./country')
 
 const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
-
-router.use('/countries', countries);
-router.use('/activity', activity);
+router.use('/', countryRouter)
 
 module.exports = router;
